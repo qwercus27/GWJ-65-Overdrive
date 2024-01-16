f34eb7c6430bad211f0149bce6c8de7e7d1ad395
qwercus27 <flyingemu@gmail.com>
2024-01-15 19:16:51 -0800

    Level System, global.gd
    Renamed player_body to player. Created Level class and two inherited scenes: Level_1 and Level_2. Created a function to switch levels in main.gd. Added a marker for DangerZone start position.

---
+++ preload/global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var current_level
+
+func _ready():
+	current_level = load("res://scenes/levels/level_1.tscn")
+	
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,11 +1,29 @@
 extends Node
 
+var level
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Player.position = $Marker2D.position
-
+	
+	level = Global.current_level.instantiate()
+	add_child(level)
+	$Player.position = level.get_node("StartPosition").position
+	$DangerZone.position = level.get_node("DangerPosition").position
+	$DangerZone.z_index = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
 	$Camera2D.position.x = $Player.position.x
+	
+	if(Input.is_action_just_pressed("1")):
+		change_level("1")
+	if(Input.is_action_just_pressed("2")):
+		change_level("2")
+
+func change_level(level_id):
+	level.queue_free()
+	Global.current_level = load("res://scenes/levels/level_" + level_id + ".tscn")
+	#level = Global.current_level.instantiate()
+	#add_child(level)
+	_ready()
